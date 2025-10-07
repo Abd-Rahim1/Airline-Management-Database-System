6b458170e158a04e32510b5dc973d39885abbe0b
Abd Rahim Mojbani <a.mojbani@pristini-international.tn>
2025-10-07 21:51:58 +0100

    feat(config): add database configuration placeholders #config

---
+++ config.py
@@ -0,0 +1,6 @@
+DB_CONFIG = {
+    "user": "",          # change this to your MySQL username
+    "password": "",          # change this to your MySQL password
+    "host": "localhost",
+    "database": "airline_db"
+}
